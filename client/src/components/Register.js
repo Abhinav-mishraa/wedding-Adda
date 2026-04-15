161edf94372070966221fe0be01b77f1629b4e7d
Abhinav-mishraa <abhinavmishra9589@gmail .com>
2026-04-16 00:40:53 +0530

    changed

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -22,7 +22,7 @@ const Register = () => {
 
     try {
       const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://wedding-adda-backend.onrender.com'  // Replace with your actual backend URL
+  ? 'https://wedding-adda-backend.onrender.com'   
   : 'http://localhost:5000';
 
 const response = await fetch(`${API_BASE_URL}/api/auth/register`, {
